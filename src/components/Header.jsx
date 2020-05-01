b4cd8dffb03e70a8b5b7729cffc05d6972767854
Chris <chris.antonello@icloud.com>
2020-05-01 22:37:13 +0200

    Removed: login from the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,19 +17,12 @@ export default class Header extends Component {
         {authenticated ? (
           <li onClick={this._handleLogoutClick}>Logout</li>
         ) : (
-          <li onClick={this._handleSignInClick}>Login</li>
+          <li></li>
         )}
       </ul>
     );
   }
-
-  _handleSignInClick = () => {
-    // Authenticate using via passport api in the backend
-    // Open Twitter login page
-    // Upon successful login, a cookie session will be stored in the client
-    window.open("http://localhost:4000/auth/twitter", "_self");
-  };
-
+  
   _handleLogoutClick = () => {
     // Logout using Twitter passport api
     // Set authenticated state to false in the HomePage
